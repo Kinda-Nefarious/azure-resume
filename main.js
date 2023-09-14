7822dcfe8965dec3f4c51a43e5b955848c0b1cc3
Kinda-Nefarious <rufaronyakudya@gmail.com>
2023-09-14 13:08:20 +0200

    Edited index variable Added main.js visitor counter code

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
-    $.getJSON("https://us-central1-cloud-res-398110.cloudfunctions.net/cloudresume-visitor-count", function(data){
-        $("#visitor-count").text(data.count);
-        $('[data-toggle="counter-up"]').counterUp({
-            delay: 10,
-            time: 1000
-        });
-    }).fail(function(){
-        console.log("An error has occurred while fetching the visitor count.");
-    });
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
 });
+
+
+const localApi = '';
+const functionApi = ''; 
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+      });
+    return count;
+}
